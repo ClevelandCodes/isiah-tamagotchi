ae4028241c3881061f88c0c9e2e6b93e55cd6af6
Isiah Donner <donner.isiah@gmail.com>
2016-04-06 14:43:57 -0400

    first commit

---
+++ Tamagotchi/NewTamagotchi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class NewTamagotchi
+    {
+        // Declare variables and make them private..
+        private string Name = "N/A";
+        private string Gender = "N/A";
+        private int Fullness = 0;
+        private int Happiness = 0;
+
+        // Constructor - a blueprint to follow..
+        public NewTamagotchi(string name, string gender, int fullness, int happines)
+        {
+            // Assign to private attribute, lowercase variables can be anything you want and they are just variable names..
+            Name = name;
+            Gender = gender;
+            Fullness = fullness;
+            Happiness = happines;
+        }
+        // Getter methods (functions) - used to drag the private attributes into the public..
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetGender()
+        {
+            return Gender;
+        }
+
+        public int GetFullness()
+        {
+            return Fullness;
+        }
+
+        public int GetHappiness()
+        {
+            return Happiness;
+        }
+
+        public int Pet()
+        {
+            Happiness = Happiness + 1;
+            return Happiness;
+        }
+
+        public int Feed()
+        {
+            Fullness = Fullness + 1;
+            return Fullness;
+        }
+   
+        // Void: does not return any value when the method is called..
+        public void DoStuff()
+        {
+            // Initializes a new instance of the Random class, using a time-dependent default seed value, Represents a pseudo-random number generator..
+            Random rnd = new Random();
+            // Same as  Happiness = Happiness - rnd.Next(10)..
+            // rnd.Next(10) - max number to be decremented by..
+            Happiness -= rnd.Next(10);
+            // Same as  Fullness = Fullness - rnd.Next(10)..
+            // rnd.Nect(10) can also hold two values.. Ex: rnd.Next(1, 10) with 1 being the minimum number and 10 being the max number
+            Fullness -= rnd.Next(10);
+        }
+    }
+}
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // New instance (copy of the class)..
+            NewTamagotchi t1 = new NewTamagotchi("Lightning", "Male", 100, 100);  
+            
+            while (t1.GetFullness() > 0 && t1.GetHappiness() > 0)
+            {
+                Console.WriteLine("Would you want to feed or pet {0}?", t1.GetName());
+                string action = Console.ReadLine();
+
+                // Makes user reponse not case-sensitive..
+                action = action.ToUpper();
+                action = action.ToLower();
+
+                if (action == "pet")
+                {
+                    Console.WriteLine("You have petted {0}",  t1.GetName() + " " + t1.GetHappiness());
+                }
+                else if (action == "feed")
+                {
+                    Console.WriteLine("You have just fed {0}",  t1.GetName() + " " + t1.GetFullness());
+                }
+                else
+                {
+                    Console.WriteLine("Either feed or pet {0}", t1.GetName());
+                }
+
+                t1.DoStuff();
+
+                if (t1.GetFullness() <= 0 || t1.GetHappiness() <= 0)
+                {
+                    Console.WriteLine("You have killed {0}", t1.GetName() + " -_- ");
+                   
+                    // break keyword ends the program..
+                    break;
+                }
+            }
+        }
+    }
+}
